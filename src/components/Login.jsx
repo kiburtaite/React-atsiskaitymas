76b4e06b7014781e2c7c79a9729ba9d612603ab4
kiburtaite <kiburtaite@gmail.com>
2022-07-07 11:27:34 +0300

    login, register fomrs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,23 @@
 import Nav from './Nav';
 
 const Login = () => {
+
+const flogin = (e) => {
+    e.preventDefault();
+    fetch("https://autumn-delicate-wilderness.glitch.me/v1/auth/login")
+    .then(res => res.json())
+    .then(data => console.log(data))
+};
+
     return (
         <div>
             <Nav />
             <h1>Login</h1>
+            <form onSubmit={flogin}>
+                <input type="email" name="email" placeholder="Įveskite el.paštą"/>
+                <input type="password" name="password" placeholder="įveskite slaptažodį"/>
+                <button type="submit">Prisijungti</button>
+            </form>
         </div>
     )
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,32 @@
 import Nav from './Nav';
 
 const Register = () => {
+
+const fregister = (e) => {
+    e.preventDefault();
+    const newUser = {
+        email: e.target.email.value,
+        password: e.target.password.value
+    }
+    fetch("https://autumn-delicate-wilderness.glitch.me/v1/auth/register", {
+      method : "POST",
+      headers : {
+        "Content-type" : "application/json",
+        "Accept" : "application/json plain/text "
+      },
+      body: JSON.stringify(newUser)
+    })
+};
+
     return (
         <div>
             <Nav />
             <h1>Register</h1>
+            <form onSubmit={fregister}>
+                <input type="email" name="email" placeholder="Įveskite el.paštą"/>
+                <input type="password" name="password" placeholder="įveskite slaptažodį"/>
+                <button type="submit">Registruotis</button>
+            </form>
         </div>
     )
 };
