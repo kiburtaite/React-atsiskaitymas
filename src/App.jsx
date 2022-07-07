2676a37caafd7ce847bec77aea9c6822c6b654e4
kiburtaite <kiburtaite@gmail.com>
2022-07-07 09:18:22 +0300

    starter app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
