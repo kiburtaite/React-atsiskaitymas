ce439eae4d7ead46c8892611505a4d93c4de2374
kiburtaite <kiburtaite@gmail.com>
2022-07-08 05:24:26 +0300

    add new entry

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import Nav from './Nav';
 
 const Add = () => {
+
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const addEntry = (e) => {
+        e.preventDefault();
+        const newEntry = {
+            title: e.target.title.value,
+            description: e.target.description.value
+        };
+        fetch("https://autumn-delicate-wilderness.glitch.me/v1/content/skills", {
+        method : "POST",
+        headers : {
+            "Content-type" : "application/json",
+            "Accept" : "application/json plain/text ",
+            "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(newEntry)
+        })
+    }; 
+
     return (
         <div>
             <Nav />
             <h1>Add</h1>
+            <form onSubmit={addEntry}>
+                <input type="text" name="title" placeholder="Įveskite pavadinimą"/>
+                <input type="textarea" name="description" placeholder="Įveskite aprašymą"/>
+                <button type="submit">Pridėti</button>
+            </form>
         </div>
     )
 };
